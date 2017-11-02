5fd9fdddbd61cd2ddbfff7b4216341d4f1051409
黄步欢 <buhuanhuang@gmail.com>
2017-11-03 00:10:11 +0800

    添加第九题解法

--- src/main/java/demo/Solution9.java
+++ src/main/java/demo/Solution9.java
@@ -8,4 +8,17 @@ package main.java.demo;
  */
 public class Solution9 {
     
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int result = 0;
+        int origin = x;
+        while (x > 0) {
+            result = result * 10 + x % 10;
+            x /= 10;
+        }
+        return origin == result;
+    }
+    
 }
