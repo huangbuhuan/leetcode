51bdc07e8bbf69127ef2a80e8b98ead21f1191bd
黄步欢 <buhuanhuang@gmail.com>
2017-11-03 00:58:56 +0800

    添加657题解法

---
+++ src/main/java/demo/Solution657.java
@@ -0,0 +1,36 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/3
+ */
+public class Solution657 {
+    
+    public boolean judgeCircle(String moves) {
+        if (moves == null || moves.length() == 0) {
+            return true;
+        }
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < moves.length(); i++) {
+            switch (moves.charAt(i)) {
+                case 'R':
+                    x++;
+                    break;
+                case 'L':
+                    x--;
+                    break;
+                case 'U':
+                    y++;
+                    break;
+                case 'D':
+                    y--;
+                    break;
+                default:
+                    return false;
+            }
+        }
+        return x == 0 && y == 0;
+    }
+}
