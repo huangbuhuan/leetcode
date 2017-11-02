301bc72f4ae70ec9c17709eb11d803b00c452ccf
黄步欢 <buhuanhuang@gmail.com>
2017-11-03 00:40:21 +0800

    添加461解法

--- src/main/java/demo/Solution14.java
+++ src/main/java/demo/Solution14.java
@@ -1,9 +1,23 @@
 package main.java.demo;
 
 /**
+ * 第十四题 查找字符串数组中最长的公共前缀字符串。
  * @author hbh
  * @version 1.0.0
  * @since 2017/11/3
  */
 public class Solution14 {
+    
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        
+        return "";
+    }
+    
+    public static void main(String[] args) {
+           
+    }
+    
 }
---
+++ src/main/java/demo/Solution461.java
@@ -0,0 +1,28 @@
+package main.java.demo;
+
+/**
+ * 第461题 计算两个数对应比特不同的位置数
+ * ===> 0110 1001
+ * <=== 4
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/3
+ */
+public class Solution461 {
+    
+    public int hammingDistance(int x, int y) {
+        if (x == y) {
+            return 0;
+        }
+        int z = x ^ y;
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((z & 1) != 0) {
+                result++;
+            }
+            z >>= 1;
+        }
+        return result;
+    }
+    
+}
