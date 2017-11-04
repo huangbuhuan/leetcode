8d2accdb64e5cb8a055fbab892f550b3efcdce00
黄步欢 <buhuanhuang@gmail.com>
2017-11-04 13:32:45 +0800

    添加500题解法

--- src/main/java/demo/Solution500.java
+++ src/main/java/demo/Solution500.java
@@ -6,4 +6,40 @@ package main.java.demo;
  * @since 2017/11/4
  */
 public class Solution500 {
+    
+    public String[] findWords(String[] words) {
+        String one = "qwertyuiopQWERTYUIOP";
+        String two = "asdfghjklASDFGHJKL";
+        String three = "zxcvbnmZXCVBNM";
+        String[] tmp = new String[words.length];
+        int index = 0;
+        for (String word : words) {
+            int a = 0;
+            boolean flag = true;
+            for (int i = 0; i < word.length(); i++) {
+                if (one.indexOf(word.charAt(i)) != -1 && (a == 0 || a == 1)) {
+                    a = 1;
+                } else if (two.indexOf(word.charAt(i)) != -1 && (a == 0 || a == 2)) {
+                    a = 2;
+                } else if (three.indexOf(word.charAt(i)) != -1 && (a == 0 || a == 3)) {
+                    a = 3;
+                } else {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                tmp[index] = word;
+                index ++;
+            }
+            flag = true;
+            a = 0;
+        }
+        String[] result = new String[index];
+        for (int i = 0; i < index; i ++) {
+            result[i] = tmp[i];
+        }
+        return result;
+    }
+    
 }
