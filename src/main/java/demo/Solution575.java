3dddef53d50516ca5db9f6511baa4353144e5fb2
黄步欢 <buhuanhuang@gmail.com>
2017-11-05 18:16:02 +0800

    添加575题

---
+++ src/main/java/demo/Solution575.java
@@ -0,0 +1,25 @@
+package main.java.demo;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/5
+ */
+public class Solution575 {
+    
+    public int distributeCandies(int[] candies) {
+        Arrays.sort(candies);
+        Set<Integer> categories = new HashSet<>();
+        for (int i = 0; i < candies.length; i++) {
+            categories.add(candies[i]);
+        }
+        int size = categories.size();
+        int half = candies.length/2;
+        return size > half ? half : size ;
+    }
+    
+}
