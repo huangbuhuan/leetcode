64d4bec5790d32a2ef64828246da07119b33866a
黄步欢 <buhuanhuang@gmail.com>
2017-11-05 17:01:46 +0800

    添加682题

---
+++ src/main/java/demo/Solution682.java
@@ -0,0 +1,14 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/5
+ */
+public class Solution682 {
+    
+    public int calPoints(String[] ops) {
+        return 1;
+    }
+    
+}
