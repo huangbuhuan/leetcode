6ace24b393ecb21914e01f5a95ba17544d442755
黄步欢 <buhuanhuang@gmail.com>
2017-11-07 22:03:10 +0800

    添加693题

---
+++ src/main/java/demo/Solution693.java
@@ -0,0 +1,12 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/7
+ */
+public class Solution693 {
+    public boolean hasAlternatingBits(int n) {
+        
+    }
+}
