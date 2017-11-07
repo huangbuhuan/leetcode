eb0d8a0b75eeeebcb27a10879c85034bb3e88032
黄步欢 <buhuanhuang@gmail.com>
2017-11-07 22:53:03 +0800

    添加485解法

--- src/main/java/demo/Solution485.java
+++ src/main/java/demo/Solution485.java
@@ -6,7 +6,21 @@ package main.java.demo;
  * @since 2017/11/7
  */
 public class Solution485 {
+    
     public int findMaxConsecutiveOnes(int[] nums) {
-        
+        int result = 0;
+        int tmp = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                tmp++;
+                if (tmp >= result) {
+                    result = tmp;
+                }
+            } else {
+                tmp = 0;
+            }
+        }
+        return result;
     }
+    
 }
