b804514a6ef0281268f7b3a8b05cc340279d741c
黄步欢 <buhuanhuang@gmail.com>
2017-11-09 00:27:02 +0800

    添加695题

---
+++ src/main/java/demo/Solution695.java
@@ -0,0 +1,14 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/9
+ */
+public class Solution695 {
+    
+    public int maxAreaOfIsland(int[][] grid) {
+        return 1;
+    }
+    
+}
