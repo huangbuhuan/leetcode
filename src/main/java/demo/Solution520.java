18704b31b8467419fb0ed494cff9f9a7d70d2377
黄步欢 <buhuanhuang@gmail.com>
2017-11-09 22:26:40 +0800

    添加520题

---
+++ src/main/java/demo/Solution520.java
@@ -0,0 +1,9 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/9
+ */
+public class Solution520 {
+}
--- src/main/java/demo/Solution717.java
+++ src/main/java/demo/Solution717.java
@@ -7,7 +7,7 @@ package main.java.demo;
  */
 public class Solution717 {
     public boolean isOneBitCharacter(int[] bits) {
-        int i = 0
+        int i = 0;
         for (; i < bits.length - 1;) {
             if (bits[i] == 1) {
                 i += 2;
