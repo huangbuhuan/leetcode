f605568f66bcdd12fa1054bcc292b2af4c3f7c3d
黄步欢 <buhuanhuang@gmail.com>
2017-11-09 22:51:13 +0800

    添加520解法

--- src/main/java/demo/Solution520.java
+++ src/main/java/demo/Solution520.java
@@ -6,4 +6,21 @@ package main.java.demo;
  * @since 2017/11/9
  */
 public class Solution520 {
+    public boolean detectCapitalUse(String word) {
+        if ("USA".equals(word)) {
+            return true;
+        }
+        int count = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) <= 90) {
+                count++;
+            }
+        }
+        if (count == word.length() || count ==0) {
+            return true;
+        } else if (count == 1 && word.charAt(0) <= 90) {
+            return true
+        }
+        return false;
+    }
 }
