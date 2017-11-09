637f9b7ddfb8684837b76f6dc38374c30f57b86b
黄步欢 <buhuanhuang@gmail.com>
2017-11-09 22:15:13 +0800

    添加717

---
+++ src/main/java/demo/Solution717.java
@@ -0,0 +1,12 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/9
+ */
+public class Solution717 {
+    public boolean isOneBitCharacter(int[] bits) {
+        return false;
+    }
+}
