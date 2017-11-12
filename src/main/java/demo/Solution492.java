21fe132b77d4b693b014945692ceca25a49cd202
黄步欢 <buhuanhuang@gmail.com>
2017-11-12 22:09:11 +0800

    添加492题解法

--- src/main/java/demo/Solution492.java
+++ src/main/java/demo/Solution492.java
@@ -6,4 +6,21 @@ package main.java.demo;
  * @since 2017/11/12
  */
 public class Solution492 {
+    
+    public int[] constructRectangle(int area) {
+        int l = (int) Math.sqrt(area);
+        int w = l;
+        while (l * w != area) {
+            if (l * w > area) {
+                w--;
+            } else {
+                l++;
+            }
+        }
+        int[] result = new int[2];
+        result[0] = l;
+        result[1] = w;
+        return result;
+    }
+    
 }
