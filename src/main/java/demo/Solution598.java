4a8839296e57de3131945f1d9cb03c07ff41830e
黄步欢 <buhuanhuang@gmail.com>
2017-11-12 22:10:25 +0800

    添加598题

---
+++ src/main/java/demo/Solution598.java
@@ -0,0 +1,14 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/12
+ */
+public class Solution598 {
+    
+    public int maxCount(int m, int n, int[][] ops) {
+        return 1;
+    }
+    
+}
