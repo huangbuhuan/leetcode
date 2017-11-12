31a2a92c7ebbdd7a23f51c3ec5e6fbbaa273b8e7
黄步欢 <buhuanhuang@gmail.com>
2017-11-12 21:51:32 +0800

    添加538解法

--- src/main/java/demo/Solution538.java
+++ src/main/java/demo/Solution538.java
@@ -7,8 +7,23 @@ package main.java.demo;
  */
 public class Solution538 {
     
+    int sum = 0;
     public TreeNode convertBST(TreeNode root) {
-        return null;
+        if (root == null) {
+            return null;
+        }
+        
+        return root;
+    }
+    
+    private void add(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        add(root.right);
+        root.val += sum;
+        sum = root.val;
+        add(root.left);
     }
     
 }
