e609c0a84002042d68ee5c8cc464be771526f1f4
黄步欢 <buhuanhuang@gmail.com>
2017-11-12 23:52:39 +0800

    添加697题

---
+++ src/main/java/demo/Solution697.java
@@ -0,0 +1,14 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/12
+ */
+public class Solution697 {
+    
+    public int findShortestSubArray(int[] nums) {
+        return 1;
+    }
+    
+}
