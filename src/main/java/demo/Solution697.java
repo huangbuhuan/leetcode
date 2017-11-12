d896baca4397f2e81227c4df56db9ebfb14914d2
黄步欢 <buhuanhuang@gmail.com>
2017-11-13 00:33:34 +0800

    添加697题解法

--- src/main/java/demo/Solution697.java
+++ src/main/java/demo/Solution697.java
@@ -1,5 +1,9 @@
 package main.java.demo;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author hbh
  * @version 1.0.0
@@ -8,7 +12,23 @@ package main.java.demo;
 public class Solution697 {
     
     public int findShortestSubArray(int[] nums) {
-        return 1;
+        Map<Integer, Integer> left = new HashMap<>();
+        Map<Integer, Integer> count = new HashMap<>();
+        Map<Integer, Integer> right = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            count.put(num, count.getOrDefault(num, 0) + 1);
+            left.putIfAbsent(num, i);
+            right.put(num, i);
+        }
+        int result = 50000;
+        int max = Collections.max(count.values());
+        for (Integer num : count.keySet()) {
+            if (count.get(num) == max) {
+                result = Math.min(right.get(num) - left.get(num) + 1, result);
+            }
+        }
+        return result;
     }
     
 }
