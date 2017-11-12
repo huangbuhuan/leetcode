7ea2750c281c8d20efd888ad321cc04c7cbff207
黄步欢 <buhuanhuang@gmail.com>
2017-11-12 13:33:42 +0800

    添加512题

---
+++ src/main/java/demo/Solution512.java
@@ -0,0 +1,16 @@
+package main.java.demo;
+
+import java.util.List;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/12
+ */
+public class Solution512 {
+    
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        return null;
+    }
+    
+}
