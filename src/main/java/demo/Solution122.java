2f662abacdb4fc31bea72f5d1dfc3b253aa1354a
黄步欢 <buhuanhuang@gmail.com>
2017-11-13 21:45:42 +0800

    添加122题

---
+++ src/main/java/demo/Solution122.java
@@ -0,0 +1,13 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/13
+ */
+public class Solution122 {
+    
+    public int maxProfit(int[] prices) {
+        return 1;
+    }
+}
