ad6a29f9ef0b341458ee2b8c42a77320388870c9
黄步欢 <buhuanhuang@gmail.com>
2017-11-13 20:55:43 +0800

    添加453题

---
+++ src/main/java/demo/Solution453.java
@@ -0,0 +1,14 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/13
+ */
+public class Solution453 {
+    
+    public int minMoves(int[] nums) {
+        return 1;
+    }
+    
+}
