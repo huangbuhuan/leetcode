c4a2058a15f166e82011432683bee93a2f1f0fa1
黄步欢 <buhuanhuang@gmail.com>
2017-11-13 21:53:26 +0800

    添加404题

---
+++ src/main/java/demo/Solution404.java
@@ -0,0 +1,14 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/13
+ */
+public class Solution404 {
+    
+    public int sumOfLeftLeaves(TreeNode root) {
+        return 1;
+    }
+    
+}
