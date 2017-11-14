0483e835d21487b527e531cf4a745f0b7b38d90e
黄步欢 <buhuanhuang@gmail.com>
2017-11-14 21:14:30 +0800

    添加530题

---
+++ src/main/java/demo/Solution530.java
@@ -0,0 +1,13 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/14
+ */
+public class Solution530 {
+    
+    public int getMinimumDifference(TreeNode root) {
+        return 1;
+    }
+}
