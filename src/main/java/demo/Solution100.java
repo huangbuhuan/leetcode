c17607cc767741cb1f3e2bcca187d11248f16193
黄步欢 <buhuanhuang@gmail.com>
2017-11-15 22:21:21 +0800

    添加110题

--- src/main/java/demo/Solution100.java
+++ src/main/java/demo/Solution100.java
@@ -6,4 +6,16 @@ package main.java.demo;
  * @since 2017/11/15
  */
 public class Solution100 {
+    
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p != null && q != null) {
+            if (p.val != q.val) {
+                return false;
+            } else {
+                return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+            }
+        }
+        return p == null && q == null;
+    }
+    
 }
