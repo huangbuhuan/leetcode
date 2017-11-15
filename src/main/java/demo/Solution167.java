da0129560eeb7778295cc548720f535ab7ab79bc
黄步欢 <buhuanhuang@gmail.com>
2017-11-15 21:23:57 +0800

    添加167题

--- src/main/java/demo/Solution167.java
+++ src/main/java/demo/Solution167.java
@@ -1,5 +1,8 @@
 package main.java.demo;
 
+
+import java.util.*;
+
 /**
  * @author hbh
  * @version 1.0.0
@@ -8,6 +11,20 @@ package main.java.demo;
 public class Solution167 {
     
     public int[] twoSum(int[] numbers, int target) {
+        int i = 0;
+        int j = numbers.length - 1;
+        int[] result = new int[2];
+        while (i < j) {
+            if (numbers[i] + numbers[j] > target) {
+                j--;
+            } else if (numbers[i] + numbers[j] < target) {
+                i++;
+            } else {
+                result[0] = i + 1;
+                result[1] = j + 1;
+                return result;
+            }
+        }
         return null;
     }
 }
