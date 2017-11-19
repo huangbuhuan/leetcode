565478860dac02fd6bf916c55fc6bf5d2b719ce7
黄步欢 <buhuanhuang@gmail.com>
2017-11-19 21:11:15 +0800

    添加543题

---
+++ src/main/java/demo/Solution543.java
@@ -0,0 +1,12 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/19
+ */
+public class Solution543 {
+    public int diameterOfBinaryTree(TreeNode root) {
+        return 1;
+    }
+}
