0318b4c6703ed6177b58ab248ae2a6c4b816c447
黄步欢 <buhuanhuang@gmail.com>
2017-11-20 21:43:01 +0800

    添加350题解法

--- src/main/java/demo/Solution350.java
+++ src/main/java/demo/Solution350.java
@@ -1,6 +1,7 @@
 package main.java.demo;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -11,7 +12,29 @@ import java.util.List;
 public class Solution350 {
     
     public int[] intersect(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null) {
+            return null;
+        }
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0, j = 0; i < nums1.length && j < nums2.length;) {
+            if (nums1[i] == nums2[j]) {
+                result.add(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
         
+        int[] nums = new int[result.size()];
+        int index = 0;
+        for (Integer i : result) {
+            nums[index++] = i;
+        }
         return nums;
     }
 }
