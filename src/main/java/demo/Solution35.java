4d73de1d1cc8fce1105b3bcd51fcc48a17405a39
黄步欢 <buhuanhuang@gmail.com>
2017-11-20 21:45:06 +0800

    添加35题

---
+++ src/main/java/demo/Solution35.java
@@ -0,0 +1,14 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/20
+ */
+public class Solution35 {
+    
+    public int searchInsert(int[] nums, int target) {
+        return 1;
+    }
+    
+}
