972295be3d9d1b41cb788b1975c9656249d6112b
黄步欢 <buhuanhuang@gmail.com>
2017-11-21 20:31:30 +0800

    添加415题

---
+++ src/main/java/demo/Solution415.java
@@ -0,0 +1,14 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/21
+ */
+public class Solution415 {
+    
+    public String addStrings(String num1, String num2) {
+        return "";
+    }
+    
+}
