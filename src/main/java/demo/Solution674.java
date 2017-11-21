5288c894295056c9f8c8d4b7f2d9739f8c975a55
黄步欢 <buhuanhuang@gmail.com>
2017-11-21 20:26:57 +0800

    添加674题

---
+++ src/main/java/demo/Solution674.java
@@ -0,0 +1,14 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/21
+ */
+public class Solution674 {
+    
+    public int findLengthOfLCIS(int[] nums) {
+        return 1;
+    }
+    
+}
