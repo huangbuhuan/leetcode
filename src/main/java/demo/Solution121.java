3f05f1dbff2b67600f1312a96f55b3914a13282b
黄步欢 <buhuanhuang@gmail.com>
2017-11-22 22:59:31 +0800

    添加121题解法

--- src/main/java/demo/Solution121.java
+++ src/main/java/demo/Solution121.java
@@ -8,7 +8,23 @@ package main.java.demo;
 public class Solution121 {
     
     public int maxProfit(int[] prices) {
-        return 1;
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int maxProfit = 0;
+        int min = prices[0];
+        int tmp;
+        for (int i = 0; i < prices.length; i++) {
+            if (min > prices[i]) {
+                min = prices[i];
+            } else {
+                tmp = prices[i] - min;
+                if (tmp > maxProfit) {
+                    maxProfit = tmp;
+                }
+            }
+        }
+        return maxProfit;
     }
     
 }
