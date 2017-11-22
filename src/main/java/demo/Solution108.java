55d526053dfbd7d925e4510165cf1e81d989496f
黄步欢 <buhuanhuang@gmail.com>
2017-11-22 20:38:17 +0800

    添加108题

---
+++ src/main/java/demo/Solution108.java
@@ -0,0 +1,14 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/22
+ */
+public class Solution108 {
+    
+    public int maxProfit(int[] prices) {
+        return 1;
+    }
+    
+}
