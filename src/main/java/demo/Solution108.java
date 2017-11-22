5cbf126389e83a90ebbeff7402263257b60338b4
黄步欢 <buhuanhuang@gmail.com>
2017-11-22 21:06:47 +0800

    添加108题解法

--- src/main/java/demo/Solution108.java
+++ src/main/java/demo/Solution108.java
@@ -7,8 +7,19 @@ package main.java.demo;
  */
 public class Solution108 {
     
-    public int maxProfit(int[] prices) {
-        return 1;
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return builder(nums, 0, nums.length - 1);
+    }
+    
+    private TreeNode builder(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int tmp = (left + right) / 2;
+        TreeNode root = new TreeNode(nums[tmp]);
+        root.left = builder(nums, left, tmp -1);
+        root.right = builder(nums, tmp + 1, right);
+        return root;
     }
     
 }
