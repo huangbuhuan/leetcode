5e84c4fdc994ee79927f8fe76889c5579332e5e6
黄步欢 <buhuanhuang@gmail.com>
2017-11-23 18:24:35 +0800

    添加628题

---
+++ src/main/java/demo/Solution628.java
@@ -0,0 +1,14 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/23
+ */
+public class Solution628 {
+    
+    public int maximumProduct(int[] nums) {
+        return 1;
+    }
+    
+}
