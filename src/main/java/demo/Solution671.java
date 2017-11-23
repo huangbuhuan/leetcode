fa63a323b5db81631b194b5fb7b6af7e8426a416
黄步欢 <buhuanhuang@gmail.com>
2017-11-23 18:25:37 +0800

    添加671题

---
+++ src/main/java/demo/Solution671.java
@@ -0,0 +1,14 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/23
+ */
+public class Solution671 {
+    
+    public int findSecondMinimumValue(TreeNode root) {
+        return 1;
+    }
+    
+}
