54ee4cb2072d2f5c0095c0bac5936f2e29cefdc0
黄步欢 <buhuanhuang@gmail.com>
2017-11-23 21:39:57 +0800

    添加671题解法

--- src/main/java/demo/Solution671.java
+++ src/main/java/demo/Solution671.java
@@ -7,8 +7,25 @@ package main.java.demo;
  */
 public class Solution671 {
     
+    private int smallest = Integer.MAX_VALUE;
+    private int secondMin = Integer.MAX_VALUE;
+    
     public int findSecondMinimumValue(TreeNode root) {
-        return 1;
+        find(root);
+        return secondMin != Integer.MAX_VALUE ? secondMin : -1;
+    }
+    
+    private void find(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        if (root.val < smallest) {
+            smallest = root.val;
+        } else if (root.val != smallest && root.val < secondMin) {
+            secondMin = root.val;
+        }
+        find(root.left);
+        find(root.right);
     }
     
 }
