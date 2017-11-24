492efadc52b9aeb29296e44ba5a83a33ba616533
黄步欢 <buhuanhuang@gmail.com>
2017-11-24 20:52:18 +0800

    添加724题

---
+++ src/main/java/demo/Solution724.java
@@ -0,0 +1,14 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/24
+ */
+public class Solution724 {
+    
+    public int pivotIndex(int[] nums) {
+        return 1;
+    }
+    
+}
