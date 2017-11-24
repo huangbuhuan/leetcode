14c25a9f51993feecbb3e012cf4f8f2601ec17b6
黄步欢 <buhuanhuang@gmail.com>
2017-11-24 21:57:41 +0800

    添加202题

--- src/main/java/demo/Solution202.java
+++ src/main/java/demo/Solution202.java
@@ -1,5 +1,10 @@
 package main.java.demo;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * @author hbh
  * @version 1.0.0
@@ -8,7 +13,26 @@ package main.java.demo;
 public class Solution202 {
     
     public boolean isHappy(int n) {
-        return false;
+        Set<Integer> set = new HashSet<>();
+        int sum = helper(n);
+        while (sum != 1) {
+            if (set.contains(sum)) {
+                return false;
+            }
+            set.add(sum);
+            sum = helper(sum);
+        }
+        return true;
     }
     
+    private int helper(int n) {
+        int tmp = 0;
+        int a;
+        while (n > 0) {
+            a = n % 10;
+            n /= 10;
+            tmp += (a * a);
+        }
+        return tmp;
+    }
 }
--- src/main/java/demo/Solution724.java
+++ src/main/java/demo/Solution724.java
@@ -1,5 +1,8 @@
 package main.java.demo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author hbh
  * @version 1.0.0
@@ -8,7 +11,20 @@ package main.java.demo;
 public class Solution724 {
     
     public int pivotIndex(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int sum = helper()
         return 1;
+    }
+    
+    private int helper(int n) {
+        int tmp = 0;
+        int a;
+        while (n > 0) {
+            a = n % 10;
+            n /= 10;
+            tmp += (a * a);
+        }
+        return tmp;
     }
     
 }
