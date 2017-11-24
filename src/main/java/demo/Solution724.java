869a6566e38df90436368afbc7ed9b71cdd3beab
黄步欢 <buhuanhuang@gmail.com>
2017-11-24 22:14:34 +0800

    添加724题解法

--- src/main/java/demo/Solution724.java
+++ src/main/java/demo/Solution724.java
@@ -1,7 +1,5 @@
 package main.java.demo;
 
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author hbh
@@ -11,20 +9,25 @@ import java.util.Map;
 public class Solution724 {
     
     public int pivotIndex(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int sum = helper()
-        return 1;
+        if (nums.length < 3) {
+            return -1;
+        }
+        int sum = 0;
+        for (int i : nums) {
+            sum += i;
+        }
+        if (sum - nums[0] == 0) {
+            return 0;
+        }
+        int tmp = 0;
+        for (int i = 1; i < nums.length; i++) {
+            tmp += nums[i - 1];
+            if (tmp == ((sum - nums[i]) / 2.0)) {
+                return i;
+            }
+        }
+        return -1;
     }
     
-    private int helper(int n) {
-        int tmp = 0;
-        int a;
-        while (n > 0) {
-            a = n % 10;
-            n /= 10;
-            tmp += (a * a);
-        }
-        return tmp;
-    }
     
 }
