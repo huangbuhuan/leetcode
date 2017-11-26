0d53c963a7681b6d80a1899b7029721cb507c3c3
黄步欢 <buhuanhuang@gmail.com>
2017-11-26 13:06:08 +0800

    添加594题

---
+++ src/main/java/demo/Solution594.java
@@ -0,0 +1,14 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/26
+ */
+public class Solution594 {
+    
+    public int findLHS(int[] nums) {
+        return 1;
+    }
+    
+}
---
+++ src/main/java/demo/Solution720.java
@@ -0,0 +1,14 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/26
+ */
+public class Solution720 {
+    
+    public String longestWord(String[] words) {
+        return "";g
+    }
+    
+}
