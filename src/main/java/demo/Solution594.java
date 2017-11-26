a17812ec4cc8e8d4151d69a0e1ff200a201b8561
黄步欢 <buhuanhuang@gmail.com>
2017-11-26 18:42:16 +0800

    添加594题解法

--- src/main/java/demo/Solution594.java
+++ src/main/java/demo/Solution594.java
@@ -1,5 +1,8 @@
 package main.java.demo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author hbh
  * @version 1.0.0
@@ -8,7 +11,24 @@ package main.java.demo;
 public class Solution594 {
     
     public int findLHS(int[] nums) {
-        return 1;
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        int result = 0;
+        int sum;
+        for (int num : map.keySet()) {
+            if (map.containsKey(num + 1)) {
+                sum = map.get(num) + map.get(num + 1);
+                if (sum > result) {
+                    result = sum;
+                }
+            }
+        }
+        return result;
     }
     
 }
--- src/main/java/demo/Solution720.java
+++ src/main/java/demo/Solution720.java
@@ -1,5 +1,8 @@
 package main.java.demo;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author hbh
  * @version 1.0.0
@@ -8,7 +11,30 @@ package main.java.demo;
 public class Solution720 {
     
     public String longestWord(String[] words) {
-        return "";g
+        if (words == null || words.length == 0) {
+            return "";
+        }
+        String result = "";
+        Set<String> wordSet = new HashSet<>();
+        for (String word : words) {
+            wordSet.add(word);
+        }
+        for (String word : words) {
+            if (word.length() > result.length() || word.length() == result.length() &&
+                word.compareTo(result) < 0) {
+                boolean flag = true;
+                for (int k = 1; k < word.length(); k++) {
+                    if (!wordSet.contains(word.substring(0, k))) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    result = word;
+                }
+            }
+        }
+        return result;
     }
     
 }
