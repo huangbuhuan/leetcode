a2e796c1fa008ad36b75ab2a4a261d4a1f518619
黄步欢 <buhuanhuang@gmail.com>
2017-11-27 19:04:07 +0800

    添加572题

---
+++ src/main/java/demo/Solution572.java
@@ -0,0 +1,14 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/27
+ */
+public class Solution572 {
+    
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        return true;
+    }
+    
+}
