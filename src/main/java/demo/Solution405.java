32bacaf421e4bb19c3eaffae44c5833209cebdb6
黄步欢 <buhuanhuang@gmail.com>
2017-11-27 19:03:24 +0800

    添加405题

---
+++ src/main/java/demo/Solution405.java
@@ -0,0 +1,14 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/27
+ */
+public class Solution405 {
+    
+    public String toHex(int num) {
+        return "";
+    }
+    
+}
