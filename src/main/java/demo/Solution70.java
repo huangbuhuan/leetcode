ebe449be640eeeea957b64d21d85b05d8f6e7e1c
黄步欢 <buhuanhuang@gmail.com>
2017-11-27 20:39:59 +0800

    添加70题

---
+++ src/main/java/demo/Solution70.java
@@ -0,0 +1,14 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/27
+ */
+public class Solution70 {
+    
+    public int climbStairs(int n) {
+        return 1;
+    }
+    
+}
