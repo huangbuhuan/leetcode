d1a8a073696da940d47056a70370ee2b38445a95
黄步欢 <buhuanhuang@gmail.com>
2017-11-27 20:39:19 +0800

    添加572题解法

--- src/main/java/demo/Solution572.java
+++ src/main/java/demo/Solution572.java
@@ -1,5 +1,7 @@
 package main.java.demo;
 
+import java.util.HashSet;
+
 /**
  * @author hbh
  * @version 1.0.0
@@ -8,7 +10,21 @@ package main.java.demo;
 public class Solution572 {
     
     public boolean isSubtree(TreeNode s, TreeNode t) {
-        return true;
+        return traverse(s, t);
+    }
+    
+    private boolean equals(TreeNode x, TreeNode y) {
+        if (x == null && y == null) {
+            return true;
+        }
+        if (x == null || y == null) {
+            return false;
+        }
+        return x.val == y.val && equals(x.left, y.left) && equals(x.right, y.right);
+    }
+    
+    private boolean traverse(TreeNode s, TreeNode t) {
+        return s != null && (equals(s, t) || traverse(s.left, t) || traverse(s.right, t));
     }
     
 }
