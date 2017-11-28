25be2c53655830be81a576283c2234087779770e
黄步欢 <buhuanhuang@gmail.com>
2017-11-28 21:13:26 +0800

    添加83题

--- src/main/java/demo/Solution83.java
+++ src/main/java/demo/Solution83.java
@@ -1,5 +1,6 @@
 package main.java.demo;
 
+
 /**
  * @author hbh
  * @version 1.0.0
@@ -7,8 +8,17 @@ package main.java.demo;
  */
 public class Solution83 {
     
+    
     public ListNode deleteDuplicates(ListNode head) {
-        return null;
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.next.val == current.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
     }
     
 }
