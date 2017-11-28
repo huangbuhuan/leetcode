b49c50acfe29168d84ee845073220149d34609f6
黄步欢 <buhuanhuang@gmail.com>
2017-11-28 20:13:31 +0800

    添加231和83题

---
+++ src/main/java/demo/Solution231.java
@@ -0,0 +1,14 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/28
+ */
+public class Solution231 {
+    
+    public boolean isPowerOfTwo(int n) {
+        return true;
+    }
+    
+}
---
+++ src/main/java/demo/Solution83.java
@@ -0,0 +1,14 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/11/28
+ */
+public class Solution83 {
+    
+    public ListNode deleteDuplicates(ListNode head) {
+        return null;
+    }
+    
+}
