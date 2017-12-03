1bf857ddb27b18e5cab9d0a6fba1411f8c2d0b6a
黄步欢 <buhuanhuang@gmail.com>
2017-12-03 22:12:54 +0800

    添加654题

---
+++ src/main/java/demo/Solution654.java
@@ -0,0 +1,13 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/12/3
+ */
+public class Solution654 {
+    
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        return null;
+    }
+}
