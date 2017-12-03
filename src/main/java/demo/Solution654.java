6dcf4575fd306411460d4c8a58739dc562398967
黄步欢 <buhuanhuang@gmail.com>
2017-12-03 22:37:10 +0800

    添加654题解法

--- src/main/java/demo/Solution654.java
+++ src/main/java/demo/Solution654.java
@@ -1,5 +1,6 @@
 package main.java.demo;
 
+
 /**
  * @author hbh
  * @version 1.0.0
@@ -8,6 +9,25 @@ package main.java.demo;
 public class Solution654 {
     
     public TreeNode constructMaximumBinaryTree(int[] nums) {
-        return null;
+        return helper(nums, 0, nums.length);
     }
+    
+    public TreeNode helper(int[] nums, int start, int end) {
+        if (start == end) {
+            return null;
+        }
+        int max = Integer.MIN_VALUE;
+        int index = -1;
+        for (int i = start; i < end; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+                index = i;
+            }
+        }
+        TreeNode root = new TreeNode(max);
+        root.left = helper(nums, start, index);
+        root.right = helper(nums, index + 1, end);
+        return root;
+    }
+    
 }
