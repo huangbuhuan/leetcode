91312ed5e1744fba81162a00dd070dacc0e338a6
黄步欢 <buhuanhuang@gmail.com>
2017-12-05 22:30:49 +0800

    简化406题

--- src/main/java/demo/Solution406.java
+++ src/main/java/demo/Solution406.java
@@ -12,13 +12,7 @@ import java.util.List;
 public class Solution406 {
 	
 	public int[][] reconstructQueue(int[][] people) {
-		Arrays.sort(people, (a, b) -> {
-			if (a[0] != b[0]) {
-				return b[0] - a[0];
-			} else {
-				return a[1] - b[1];
-			}
-		});
+		Arrays.sort(people, (a, b) -> a[0] != b[0] ? b[0] - a[0] : a[1] - b[1]);
 		List<int[]> result = new LinkedList<>();
 		for (int[] p : people) {
 			result.add(p[1], p);
