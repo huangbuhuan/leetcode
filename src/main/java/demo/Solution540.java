7a0a81653a0101a489d3ece0f4b80d82d69f1bff
黄步欢 <buhuanhuang@gmail.com>
2017-12-06 19:27:23 +0800

    添加540题

---
+++ src/main/java/demo/Solution540.java
@@ -0,0 +1,13 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/12/6
+ */
+public class Solution540 {
+    
+    public int singleNonDuplicate(int[] nums) {
+        return 1;
+    }
+}
