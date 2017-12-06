01ba98ff06dced33394f1dc6d52c742153084377
黄步欢 <buhuanhuang@gmail.com>
2017-12-06 19:30:17 +0800

    添加513题

---
+++ src/main/java/demo/Solution513.java
@@ -0,0 +1,14 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/12/6
+ */
+public class Solution513 {
+    
+    public int findBottomLeftValue(TreeNode root) {
+        return 1;
+    }
+    
+}
