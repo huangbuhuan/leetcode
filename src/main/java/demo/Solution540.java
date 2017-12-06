63018f4c65b55380ccab22237da2cbb769502792
黄步欢 <buhuanhuang@gmail.com>
2017-12-06 19:29:33 +0800

    添加540题解法

--- src/main/java/demo/Solution540.java
+++ src/main/java/demo/Solution540.java
@@ -8,6 +8,10 @@ package main.java.demo;
 public class Solution540 {
     
     public int singleNonDuplicate(int[] nums) {
-        return 1;
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
     }
 }
