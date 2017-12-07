bbe4be1974f00aafb49de102afe5f1a5987145ed
黄步欢 <buhuanhuang@gmail.com>
2017-12-07 18:45:14 +0800

    添加647题

--- src/main/java/demo/Solution513.java
+++ src/main/java/demo/Solution513.java
@@ -1,5 +1,8 @@
 package main.java.demo;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author hbh
  * @version 1.0.0
@@ -9,8 +12,27 @@ public class Solution513 {
     
     private int result = -1;
     private int dep = 1;
+    
     public int findBottomLeftValue(TreeNode root) {
-        helper(root, 1);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int size = queue.size();
+        int result = root.val;
+        TreeNode tmp;
+        while(size != 0) {
+            size = queue.size();
+            tmp = queue.element();
+            result = tmp.val;
+            while(size-- != 0) {
+                tmp = queue.poll();
+                if (tmp.left != null) {
+                    queue.add(tmp.left);
+                }
+                if (tmp.right != null) {
+                    queue.add(tmp.right);
+                }
+            }
+        }
         return result;
     }
     
---
+++ src/main/java/demo/Solution647.java
@@ -0,0 +1,13 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @version 1.0.0
+ * @since 2017/12/7
+ */
+public class Solution647 {
+	
+	public int countSubstrings(String s) {
+		return 1;
+	}
+}
