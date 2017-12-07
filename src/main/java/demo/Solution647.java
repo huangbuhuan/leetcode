7465d4848fe70ee794755121cd9d736c87ff11cb
黄步欢 <buhuanhuang@gmail.com>
2017-12-07 20:22:41 +0800

    添加647题解法

--- src/main/java/demo/Solution647.java
+++ src/main/java/demo/Solution647.java
@@ -8,6 +8,16 @@ package main.java.demo;
 public class Solution647 {
 	
 	public int countSubstrings(String s) {
-		return 1;
+	    int res = 0;
+	    int length = s.length();
+	    for (int i = 0; i< length; i++) {
+	        for (int j = 0; i - j >= 0 && i + j < length && s.charAt(i - j) == s.charAt(i + j); j++) {
+	            res ++;
+            }
+            for (int j = 0; i - 1 - j >= 0 && i + j < length && s.charAt(i - 1 - j) == s.charAt(i + j); j++) {
+                res++;
+            }
+        }
+		return res;
 	}
 }
