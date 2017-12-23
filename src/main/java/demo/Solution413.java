4033ef1acc17c48d6f64ad4d0a97d13eb51608d8
黄步欢 <buhuanhuang@gmail.com>
2017-12-23 21:47:02 +0800

    添加413题解法

--- src/main/java/demo/Solution413.java
+++ src/main/java/demo/Solution413.java
@@ -6,6 +6,24 @@ package main.java.demo;
  * @since 2017/12/23
  */
 public class Solution413 {
-
-
+	
+	public int numberOfArithmeticSlices(int[] A) {
+		int count = 0;
+		int result = 0;
+		for (int i = 0; i < A.length - 2; i ++) {
+			if (A[i] + A[i + 2] == 2 * A[i + 1]) {
+				count++;
+			} else {
+				if (count > 0) {
+					result += (1 + count) * count / 2;
+				}
+				count = 0;
+			}
+		}
+		if (count > 0) {
+			result += (1 + count) * count / 2;
+		}
+		return result;
+	}
+	
 }
