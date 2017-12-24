9db5bb3c7adda5e2e4e6e36e292d654460a7bff3
黄步欢 <buhuanhuang@gmail.com>
2017-12-24 21:44:39 +0800

    添加739题解法

--- src/main/java/demo/Solution739.java
+++ src/main/java/demo/Solution739.java
@@ -1,9 +1,23 @@
 package main.java.demo;
 
+
 /**
  * @author hbh
  * @version 1.0.0
  * @since 2017/12/24
  */
 public class Solution739 {
+    
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        for (int i = 0; i < temperatures.length; i++) {
+            for (int j = i + 1; j < temperatures.length; j++) {
+                if (temperatures[i] < temperatures[j]) {
+                    result[i] = j - i;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
 }
