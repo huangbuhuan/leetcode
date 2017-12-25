6a398a498c169dd12fab8f95d6c35815d56bb6aa
黄步欢 <buhuanhuang@gmail.com>
2017-12-25 20:30:02 +0800

    添加260题解法

--- src/main/java/demo/Solution260.java
+++ src/main/java/demo/Solution260.java
@@ -1,5 +1,7 @@
 package main.java.demo;
 
+import java.util.*;
+
 /**
  * @author hbh
  * @version 1.0.0
@@ -8,7 +10,27 @@ package main.java.demo;
 public class Solution260 {
     
     public int[] singleNumber(int[] nums) {
-        return null;
+        if (nums.length == 2) {
+            return nums;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            Integer value = map.get(num);
+            if (value != null) {
+                map.put(num, ++value);
+            } else {
+                map.put(num, 1);
+            }
+        }
+        int[] result = new int[2];
+        int tmp = 0;
+        for (Integer key : map.keySet()) {
+            if (map.get(key) == 1) {
+                result[tmp++] = key;
+            }
+        }
+        
+        return result;
     }
     
 }
