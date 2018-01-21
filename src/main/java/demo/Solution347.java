d02e1060e08a6e98134c3898ce694b9f8b25a41f
黄步欢 <buhuanhuang@gmail.com>
2018-01-21 21:09:26 +0800

    添加347题解法

--- src/main/java/demo/Solution347.java
+++ src/main/java/demo/Solution347.java
@@ -1,11 +1,30 @@
 package main.java.demo;
 
-import java.util.List;
+import java.util.*;
 
 public class Solution347 {
 
 	public List<Integer> topKFrequent(int[] nums, int k) {
-		return null;
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int num : nums) {
+			int value = map.getOrDefault(num, 0);
+			map.put(num, ++value);
+		}
+		List[] bucket = new List[nums.length + 1];
+		for (Integer key : map.keySet()) {
+			int count = map.get(key);
+			if (bucket[count] == null) {
+				bucket[count] = new ArrayList<>();
+			}
+			bucket[count].add(key);
+		}
+		List<Integer> result = new ArrayList<>();
+		for (int i = bucket.length - 1; result.size() < k; i--) {
+			if (bucket[i] != null) {
+				result.addAll(bucket[i]);
+			}
+		}
+		return result;
 	}
 
 }
