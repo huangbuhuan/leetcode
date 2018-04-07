7b0531500fd4c6c5858c85d6263d8345b857beb1
黄步欢 <i@buhuan.me>
2018-04-07 19:11:00 +0800

    添加98题解法

--- src/main/java/demo/Solution98.java
+++ src/main/java/demo/Solution98.java
@@ -3,6 +3,20 @@ package main.java.demo;
 public class Solution98 {
 
 	public boolean isValidBST(TreeNode root) {
-		return false;
+		return isValidBST(root, null, null);
 	}
+
+	private boolean isValidBST(TreeNode root, Integer min, Integer max) {
+		if (root == null) {
+			return true;
+		}
+		if ((min != null && root.val <= min) ||
+			(max != null && root.val >= max)) {
+			return false;
+		}
+
+		return isValidBST(root.left, min, root.val)
+			&& isValidBST(root.right, root.val, max);
+	}
+
 }
