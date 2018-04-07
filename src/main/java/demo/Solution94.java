5bd3e24e1d28dca91bf9b292c48d8522fbc45ab9
黄步欢 <i@buhuan.me>
2018-04-07 14:43:57 +0800

    添加94题解法

--- src/main/java/demo/Solution94.java
+++ src/main/java/demo/Solution94.java
@@ -1,4 +1,29 @@
 package main.java.demo;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Solution94 {
+
+	private List<Integer> result = new LinkedList<>();
+	private int index = 0;
+
+	public List<Integer> inorderTraversal(TreeNode root) {
+		add(result, index, root);
+		return result;
+	}
+
+	public void add(List<Integer> result, int index, TreeNode root) {
+		if (root == null) {
+			return;
+		}
+		result.add(index, root.val);
+		if (root.right != null) {
+			add(result, index + 1, root.right);
+		}
+		if (root.left != null) {
+			add(result, index, root.left);
+		}
+	}
+
 }
