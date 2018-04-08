1fdfeddf1192101d9ef14496d7eb9506189ebe47
黄步欢 <i@buhuan.me>
2018-04-08 21:56:16 +0800

    添加145题解法

--- src/main/java/demo/Solution145.java
+++ src/main/java/demo/Solution145.java
@@ -1,11 +1,29 @@
 package main.java.demo;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Solution145 {
 
+	private List<Integer> result = new LinkedList<>();
+
 	public List<Integer> postorderTraversal(TreeNode root) {
-		return null;
+		parse(root, 0);
+		return result;
+	}
+
+	private void parse(TreeNode root, int index) {
+		if (root == null) {
+			return;
+		}
+
+		result.add(index, root.val);
+		if (root.right != null) {
+			parse(root.right, index);
+		}
+		if (root.left != null) {
+			parse(root.left, index);
+		}
 	}
 
 }
