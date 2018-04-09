7ee311890eb179fa7eedc34b11d34c659d1104c0
黄步欢 <i@buhuan.me>
2018-04-09 21:25:25 +0800

    添加814题

--- src/main/java/demo/Solution230.java
+++ src/main/java/demo/Solution230.java
@@ -1,7 +1,28 @@
 package main.java.demo;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Solution230 {
+
+	private List<Integer> tree = new LinkedList<>();
+
 	public int kthSmallest(TreeNode root, int k) {
-		return 1;
+		parse(root, 0);
+		return tree.get(k - 1);
+	}
+
+	private void parse(TreeNode root, int index) {
+		if (root == null) {
+			return;
+		}
+
+		tree.add(index, root.val);
+		if (root.right != null) {
+			parse(root.right, index);
+		}
+		if (root.left != null) {
+			parse(root.left, index);
+		}
 	}
 }
---
+++ src/main/java/demo/Solution814.java
@@ -0,0 +1,8 @@
+package main.java.demo;
+
+public class Solution814 {
+
+	public TreeNode pruneTree(TreeNode root) {
+		return null;
+	}
+}
