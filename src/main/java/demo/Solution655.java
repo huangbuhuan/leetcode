fa70d2d9905cbc1360e1feb3d49964fdf22621dc
黄步欢 <i@buhuan.me>
2018-04-09 22:30:20 +0800

    添加814解法

---
+++ src/main/java/demo/Solution655.java
@@ -0,0 +1,11 @@
+package main.java.demo;
+
+import java.util.List;
+
+public class Solution655 {
+
+	public List<List<String>> printTree(TreeNode root) {
+		return null;
+	}
+
+}
--- src/main/java/demo/Solution814.java
+++ src/main/java/demo/Solution814.java
@@ -3,6 +3,21 @@ package main.java.demo;
 public class Solution814 {
 
 	public TreeNode pruneTree(TreeNode root) {
-		return null;
+		return containsOne(root) ? root : null;
+	}
+
+	private boolean containsOne(TreeNode root) {
+		if (root == null) {
+			return false;
+		}
+		boolean a1 = containsOne(root.left);
+		boolean a2 = containsOne(root.right);
+		if (!a1) {
+			root.left = null;
+		}
+		if (!a2) {
+			root.right = null;
+		}
+		return root.val == 1 || a1 || a2;
 	}
 }
