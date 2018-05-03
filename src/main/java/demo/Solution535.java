73447e9820731e77d737f90363cb8c80983d5bed
黄步欢 <i@buhuan.me>
2018-05-03 22:12:45 +0800

    添加535题答案

--- src/main/java/demo/Solution535.java
+++ src/main/java/demo/Solution535.java
@@ -3,11 +3,10 @@ package main.java.demo;
 public class Solution535 {
 
 	public String encode(String longUrl) {
-
+		return longUrl;
 	}
 
-	// Decodes a shortened URL to its original URL.
 	public String decode(String shortUrl) {
-
+		return shortUrl;
 	}
 }
