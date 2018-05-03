c6629e518266dd3a78df232bafa2c2d8dbbaad5c
黄步欢 <i@buhuan.me>
2018-05-03 22:02:15 +0800

    添加535

---
+++ src/main/java/demo/Solution535.java
@@ -0,0 +1,13 @@
+package main.java.demo;
+
+public class Solution535 {
+
+	public String encode(String longUrl) {
+
+	}
+
+	// Decodes a shortened URL to its original URL.
+	public String decode(String shortUrl) {
+
+	}
+}
