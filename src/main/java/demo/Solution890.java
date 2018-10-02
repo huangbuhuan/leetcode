a57b2e1127116eedd15e5e8fc92a10093cdef33a
黄步欢 <haizao.hbh@raycloud.com>
2018-10-02 13:04:22 +0800

    添加890题解法

--- src/main/java/demo/Solution890.java
+++ src/main/java/demo/Solution890.java
@@ -1,6 +1,10 @@
 package main.java.demo;
 
+import javax.crypto.MacSpi;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 890题
@@ -10,6 +14,30 @@ import java.util.List;
 public class Solution890 {
 
     public List<String> findAndReplacePattern(String[] words, String pattern) {
-        return null;
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            if (match(word, pattern)) {
+                result.add(word);
+            }
+        }
+        return result;
     }
+
+    private boolean match(String word, String pattern) {
+        Map<Character, Character> map1 = new HashMap<>();
+        Map<Character, Character> map2 = new HashMap<>();
+        for (int j = 0; j < word.length(); j++) {
+            char p = pattern.charAt(j);
+            char w = word.charAt(j);
+            if (!map1.containsKey(p)) map1.put(p, w);
+            if (!map2.containsKey(w)) map2.put(w, p);
+
+            if (map1.get(p) != w || map2.get(w) != p) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
