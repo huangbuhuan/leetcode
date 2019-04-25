1bcf7f4f11851a88b74578d75a57fce5e2a6c3ba
黄步欢 <i@buhuan.me>
2019-04-26 00:30:12 +0800

    resolve 1017

--- src/main/java/demo/Solution1017.java
+++ src/main/java/demo/Solution1017.java
@@ -6,6 +6,10 @@ package main.java.demo;
  */
 public class Solution1017 {
 	public String baseNeg2(int N) {
-		return "";
+		if (N == 0 || N == 1) {
+			return String.valueOf(N);
+		}
+
+		return baseNeg2(-(N >> 1)) + (N & 1);
 	}
 }
