c69e00093d75e33e286360aabaa996e483ae5bba
黄步欢 <i@buhuan.me>
2019-04-27 11:37:21 +0800

    add 841

---
+++ src/main/java/demo/Solution841.java
@@ -0,0 +1,13 @@
+package main.java.demo;
+
+import java.util.List;
+
+/**
+ * @author hbh
+ * @date 2019-04-27
+ */
+public class Solution841 {
+	public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+		return false;
+	}
+}
