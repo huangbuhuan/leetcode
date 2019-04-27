68cb2ae61afc7716d48de4338b894d48c0bd4c80
黄步欢 <i@buhuan.me>
2019-04-27 11:34:48 +0800

    resolve 495

--- src/main/java/demo/Solution495.java
+++ src/main/java/demo/Solution495.java
@@ -5,4 +5,21 @@ package main.java.demo;
  * @date 2019-04-27
  */
 public class Solution495 {
+	public int findPoisonedDuration(int[] timeSeries, int duration) {
+		if (timeSeries.length == 0) {
+			return 0;
+		}
+		int result = 0;
+		for (int i = 0; i < timeSeries.length - 1; i++) {
+			if (timeSeries[i + 1] - timeSeries[i] < duration) {
+				result += timeSeries[i + 1] - timeSeries[i];
+			} else {
+				result += duration;
+			}
+		}
+
+		result += duration;
+		return result;
+	}
+
 }
