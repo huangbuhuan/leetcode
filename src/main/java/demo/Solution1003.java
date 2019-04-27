1476ee54cb6c63a3296790eecd9603851e8c6d90
黄步欢 <i@buhuan.me>
2019-04-27 16:31:39 +0800

    resolve 1003

--- src/main/java/demo/Solution1003.java
+++ src/main/java/demo/Solution1003.java
@@ -1,5 +1,8 @@
 package main.java.demo;
 
+
+import java.util.Stack;
+
 /**
  * @author hbh
  * @date 2019-04-27
@@ -7,7 +10,30 @@ package main.java.demo;
 public class Solution1003 {
 
     public boolean isValid(String S) {
-        return false;
+        Stack<Character> stack = new Stack<>();
+        for (char c : S.toCharArray()) {
+            if (c == 'c') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                if (stack.pop() != 'b') {
+                    return false;
+                }
+
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                if (stack.pop() != 'a') {
+                    return false;
+                }
+
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
     }
 
 }
