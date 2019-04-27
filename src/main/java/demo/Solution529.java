82883f204c50e01c9f15b76b232f09db7e34b51c
黄步欢 <i@buhuan.me>
2019-04-27 20:21:42 +0800

    add 549

---
+++ src/main/java/demo/Solution529.java
@@ -0,0 +1,11 @@
+package main.java.demo;
+
+/**
+ * @author hbh
+ * @date 2019-04-27
+ */
+public class Solution529 {
+    public char[][] updateBoard(char[][] board, int[] click) {
+        return null;
+    }
+}
