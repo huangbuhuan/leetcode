17f2c16439e5ac0d50f9480b344595a16ff76314
黄步欢 <i@buhuan.me>
2019-05-06 23:20:42 +0800

    resolve 1023

--- src/main/go/main/Solution1023.go
+++ src/main/go/main/Solution1023.go
@@ -1,3 +1,34 @@
-func camelMatch(queries []string, pattern string) []bool {
+import (
+	"unicode"
+)
 
+
+func camelMatch(queries []string, pattern string) []bool {
+	result := make([]bool, len(queries))
+	patternWords := []rune(pattern)
+	wordsLength := len(patternWords)
+	fmt.Println("words", len(patternWords))
+	for i, e := range queries {
+		runes := []rune(e)
+		flag := true
+		k := 0
+		for _, v := range runes {
+			if k < wordsLength && patternWords[k] == v {
+				k++
+				continue
+			}
+			if unicode.IsLower(v) {
+				continue
+			} else {
+				flag = false
+				break
+			}
+
+		}
+		if flag && k == wordsLength {
+			result[i] = true
+		}
+	}
+
+	return result
 }
