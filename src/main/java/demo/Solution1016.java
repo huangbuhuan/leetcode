7f5732e9a75b7a2bbd8015a23a2a0dd71a52b856
黄步欢 <i@buhuan.me>
2019-08-14 23:46:55 +0800

    resolve 1016

--- src/main/java/demo/Solution1016.java
+++ src/main/java/demo/Solution1016.java
@@ -5,4 +5,12 @@ package main.java.demo;
  * @date 2019/8/14
  */
 public class Solution1016 {
+	public boolean queryString(String S, int N) {
+		for (int i = 1; i <= N; i++) {
+			if (!S.contains(Integer.toBinaryString(i))) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
